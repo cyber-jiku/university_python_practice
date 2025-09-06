4826e65e5f64146b4ec51861fb7d3e87459a5759
J𝚒𝚔𝚞⁈ <227148145+cyber-jiku@users.noreply.github.com>
2025-09-06 19:19:45 +0400

    Question 4: File Size Conversion.py

--- year-1/week-03/Question 4: File Size Conversion.py
+++ year-1/week-03/Question 4: File Size Conversion.py
@@ -5,6 +5,6 @@ file_size = eval(input("Please enter the file size in bytes: "))
 B_KB = file_size / 1024
 KB_MB = B_KB / 1024
 
-#Output to User (Size in KB and MB)
+#Output to User(Size in KB and MB)
 print("File size in KB:", B_KB)
 print("File size in MB:", KB_MB)
